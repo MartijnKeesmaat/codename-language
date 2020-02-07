eef93ee05978867d88008cec21e9ed526cb6eef2
Martijn <mpkeesmaat@gmail.com>
2020-02-07 22:49:12 +0100

    did some things

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,34 @@
 import gsap from 'gsap';
+
+const menu = document.querySelector('nav button');
+
+let active = false;
+
+menu.addEventListener('click', function() {
+  if (!active) {
+    var tl = gsap.timeline();
+    tl.to('.header-text', { duration: 0.3, autoAlpha: 0, x: 20 });
+
+    tl.to('.menu-item', {
+      x: '+=20',
+      duration: 0.3,
+      autoAlpha: 1,
+      stagger: 0.2,
+      ease: 'power2.inOut'
+    });
+    active = !active;
+  } else {
+    var tl2 = gsap.timeline();
+    tl2.to('.menu-item', {
+      x: '+=0',
+      duration: 0.3,
+      autoAlpha: 0,
+      stagger: 0.2,
+      ease: 'power2.inOut'
+    });
+
+    tl2.to('.header-text', { delay: 0.2, duration: 0.3, autoAlpha: 1, x: 0 });
+  }
+
+  document.body.classList.toggle('menu-active');
+});
